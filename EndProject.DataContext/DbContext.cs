44153e3c8d32c3a3d569af0abe2b413dd02a694c
TeymurDevv <transmissiyaservis@gmail.com>
2023-12-25 00:46:38 +0400

    DataContext Layer Created, Static Database (DbContext.cs) Created

---
+++ EndProject.DataContext/DbContext.cs
@@ -0,0 +1,24 @@
+﻿using EndProject.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndProject.DataContext
+{
+    static class DbContext
+    {
+
+        public static List<Employee> Employees { get; set; }
+        public static List<Department> Departments { get; set; }
+
+        static DbContext() 
+        {
+            
+            Employees = new List<Employee>();
+            Departments = new List<Department>();
+
+        }
+    }
+}
