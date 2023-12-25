ed47ff995287390372fe057d2e7ca058553f07b2
TeymurDevv <transmissiyaservis@gmail.com>
2023-12-25 23:10:57 +0400

    Business Layer Created, IEmployee Business Interface created with Methods (CRUD)

---
+++ EndProject.Business/Interfaces/IEmployee.cs
@@ -0,0 +1,19 @@
+﻿using EndProject.Domain;
+
+namespace EndProject.Business.Interfaces
+{
+    public interface  IEmployee
+    {
+        List<Employee> GetAll();
+        List<Employee> GetEmployeesByDepartment(string departmentName);
+        List<Employee> GetAll(string name);
+        List<Employee> GetAll(int age);
+        List<Employee> GetAll(byte departmentId);
+
+        Employee Create(Employee employee, string departmentName);
+        Employee Get(int id);
+        Employee Update(int id, Employee employee, string? departmentName);
+        Employee Delete(int id);
+
+    }
+}
