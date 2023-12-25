440b3b3acf3f7040392b7d49cc33de20e85e048e
TeymurDevv <transmissiyaservis@gmail.com>
2023-12-25 21:52:44 +0400

    IRepository Interface Created Which have BaseEntity General Methods That returns bool that describes is the Operation successfull or Fail (CRUD)

---
+++ EndProject.DataContext/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using EndProject.Domain.Common;
+namespace EndProject.DataContext.Interfaces
+{
+    public interface IRepository <T> where T : BaseEntity
+    {
+        bool Create(T entity);
+        bool Update (T entity);
+        bool Delete (T entity);
+        T Get(Predicate<T> filter);
+        List<T> GetAll(Predicate<T> filter = null);
+    }
+}
