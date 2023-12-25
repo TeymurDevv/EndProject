5030610906f32f77a36fd0f0394914bffed1768b
TeymurDevv <transmissiyaservis@gmail.com>
2023-12-25 23:24:14 +0400

    Department Business Interface created with Methods (CRUD)

---
+++ EndProject.Business/Interfaces/IDepartment.cs
@@ -0,0 +1,17 @@
+﻿using EndProject.Domain;
+
+namespace EndProject.Business.Interfaces
+{
+    public interface IDepartment
+    {
+        Department Create(Department department);
+        Department Update(int id, Department department);
+        Department Delete(int id);
+        Department Get(int id);
+
+        List<Department> GetAll();
+        List<Department> GetAll(string name);
+        List<Department> GetAll(int capacity);
+
+    }
+}
