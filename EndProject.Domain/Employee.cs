4bc6f69600f37de79180c1b96bd0b4a32a95d7f0
TeymurDevv <transmissiyaservis@gmail.com>
2024-01-05 21:09:16 +0400

    Employee Entity ToString Method updated

--- EndProject.Domain/Employee.cs
+++ EndProject.Domain/Employee.cs
@@ -8,5 +8,9 @@ namespace EndProject.Domain
         public int Age { get; set; }
         public string Adress { get; set; }
         public Department Department { get; set; }
+        public override string ToString()
+        {
+            return $"Id: {Id} Name: {Name} SurName: {SurName} Age: {Age} Adress: {Adress} Department: {Department.Name}";
+        }
     }
 }
