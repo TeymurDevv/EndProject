b6524dff3eeee0aa68037965b8dcd557e74c8717
TeymurDevv <transmissiyaservis@gmail.com>
2024-01-06 14:41:59 +0400

    Useless Using Deletion a bit Optimized code

--- EndProject/Controllers/DepartmentController.cs
+++ EndProject/Controllers/DepartmentController.cs
@@ -1,17 +1,8 @@
-﻿using EndProject.Business.Interfaces;
-using EndProject.Business.Services;
-using EndProject.DataContext.Repositories;
+﻿using EndProject.Business.Services;
 using EndProject.Domain;
 using EndProject.Helpers;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace EndProject.Controllers
 {
--- EndProject/Controllers/EmployeeController.cs
+++ EndProject/Controllers/EmployeeController.cs
@@ -2,11 +2,6 @@
 using EndProject.DataContext;
 using EndProject.Domain;
 using EndProject.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EndProject.Controllers
 {
